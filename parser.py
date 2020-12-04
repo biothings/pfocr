160a78037c1051923a05c861c4b6d046cba6ff6a
jiwen xin <kevinxin@scripps.edu>
2020-12-04 09:39:20 -0800

    update file name

--- parser.py
+++ parser.py
@@ -1,10 +1,10 @@
 import os
 import json
 
+
 def load_data(data_folder):
-    file_path = os.path.join(data_folder, "pfocr_biothings.ndjson")
+    file_path = os.path.join(data_folder, "pfocr_biothings_65k_20201203.json")
     with open(file_path) as f:
         for l in f.readlines():
             doc = json.loads(l)
             yield doc
-
